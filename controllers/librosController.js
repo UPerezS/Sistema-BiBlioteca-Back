c4452b1e51eec15b0b3124ba69652a4cb4fee377
SaulReyes-ux <79472319+SaulReyes-ux@users.noreply.github.com>
2024-07-02 13:30:16 -0600

    SGMR Se agrega el back para actualizar y eliminar libros

--- controllers/librosController.js
+++ controllers/librosController.js
@@ -28,3 +28,60 @@ exports.obtenerLibros = (req, res) => {
     res.status(200).json(results);
   });
 };
+
+
+// Función para actualizar un libro por su ID
+// Función para formatear la fecha a 'YYYY-MM-DD'
+function formatDate(fecha) {
+  // Suponiendo que 'fecha' es un string en formato 'YYYY/MM/DD':
+  const parts = fecha.split('/');
+  return `${parts[0]}-${parts[1]}-${parts[2]}`;
+}
+
+exports.actualizarLibro = (req, res) => {
+  const id = req.params.id;
+  const { titulo_libro, autor, fecha_publicacion, genero, estatus_prestamo, estatus } = req.body;
+
+  // Formatear la fecha a 'YYYY-MM-DD'
+  const fechaPublicacionFormatted = formatDate(fecha_publicacion);
+
+  const updateQuery = `UPDATE libros 
+                       SET titulo_libro = ?, autor = ?, fecha_publicacion = ?, genero = ?, estatus_prestamo = ?, estatus = ?
+                       WHERE id_libro = ?`;
+  const values = [titulo_libro, autor, fechaPublicacionFormatted, genero, estatus_prestamo, estatus, id];
+
+  db.query(updateQuery, values, (err, result) => {
+    if (err) {
+      console.error("Error al actualizar el libro:", err);
+      res.status(500).send("Error interno al actualizar el libro");
+      return;
+    }
+
+    if (result.affectedRows === 0) {
+      console.log("No se encontró ningún libro con ese ID para actualizar");
+      res.status(404).send("No se encontró ningún libro con ese ID para actualizar");
+      return;
+    }
+
+    console.log("Libro actualizado correctamente");
+    res.status(200).send("Libro actualizado correctamente");
+  });
+};
+
+
+
+exports.eliminarLibro = (req, res) => {
+  const id = req.params.id;
+
+  const deleteQuery = `DELETE FROM libros WHERE id_libro = ?`;
+
+  db.query(deleteQuery, [id], (err, result) => {
+    if (err) {
+      console.error("Error al eliminar el libro:", err);
+      res.status(500).send("Error interno al eliminar el libro");
+      return;
+    }
+    console.log("Libro eliminado correctamente");
+    res.status(200).send("Libro eliminado correctamente");
+  });
+};
--- routes/librosRoutes.js
+++ routes/librosRoutes.js
@@ -6,5 +6,9 @@ const librosController = require('../controllers/librosController');
 // Ruta para insertar un nuevo libro
 router.post('/libros', librosController.insertarLibro);
 router.get('/libros', librosController.obtenerLibros);
+// Ruta para actualizar un libro por su ID
+router.put('/libros/:id', librosController.actualizarLibro);
+// Ruta para eliminar un libro por su ID
+router.delete('/libros/:id', librosController.eliminarLibro);
 
 module.exports = router;
